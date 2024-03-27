7bab1e39044fb6c78edc7ae318d26ab64f2d853b
Paulo Lorenzo Basilio <paulolorenzobasilio@yahoo.com>
2024-03-27 16:31:12 +0800

    chore: added foreign key on dump sql

--- dbdump.sql
+++ dbdump.sql
@@ -1,79 +1,64 @@
--- MySQL dump 10.13  Distrib 5.7.16, for Linux (x86_64)
---
--- Host: localhost    Database: phptest
--- ------------------------------------------------------
--- Server version	5.7.16-0ubuntu0.16.04.1
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               8.0.36-0ubuntu0.22.04.1 - (Ubuntu)
+-- Server OS:                    Linux
+-- HeidiSQL Version:             12.6.0.6765
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `comment`
---
 
+-- Dumping database structure for phptest
+DROP DATABASE IF EXISTS `phptest`;
+CREATE DATABASE IF NOT EXISTS `phptest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `phptest`;
+
+-- Dumping structure for table phptest.comment
 DROP TABLE IF EXISTS `comment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `comment` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `body` mediumtext,
   `created_at` datetime DEFAULT NULL,
-  `news_id` int(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `news_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_news_id` (`news_id`),
+  CONSTRAINT `fk_news_id` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `comment`
---
+-- Dumping data for table phptest.comment: ~6 rows (approximately)
+INSERT INTO `comment` (`id`, `body`, `created_at`, `news_id`) VALUES
+	(1, 'i like this news', '2016-11-30 14:21:23', 1),
+	(2, 'i have no opinion about that', '2016-11-30 14:24:08', 1),
+	(3, 'are you kidding me ?', '2016-11-30 14:28:06', 1),
+	(4, 'this is so true', '2016-11-30 17:21:23', 2),
+	(5, 'trolololo', '2016-11-30 17:39:25', 2),
+	(6, 'luke i am your father', '2016-11-30 14:34:06', 3);
 
-LOCK TABLES `comment` WRITE;
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES (1,'i like this news','2016-11-30 14:21:23',1),(2,'i have no opinion about that','2016-11-30 14:24:08',1),(3,'are you kidding me ?','2016-11-30 14:28:06',1),(4,'this is so true','2016-11-30 17:21:23',2),(5,'trolololo','2016-11-30 17:39:25',2),(6,'luke i am your father','2016-11-30 14:34:06',3);
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `news`
---
-
+-- Dumping structure for table phptest.news
 DROP TABLE IF EXISTS `news`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `news` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(511) DEFAULT NULL,
   `body` mediumtext,
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `news`
---
+-- Dumping data for table phptest.news: ~3 rows (approximately)
+INSERT INTO `news` (`id`, `title`, `body`, `created_at`) VALUES
+	(1, 'news 1', 'this is the description of our fist news', '2016-11-30 14:18:23'),
+	(2, 'news 2', 'this is the description of our second news', '2016-11-30 14:24:23'),
+	(3, 'news 3', 'this is the description of our third news', '2016-12-01 04:33:23');
 
-LOCK TABLES `news` WRITE;
-/*!40000 ALTER TABLE `news` DISABLE KEYS */;
-INSERT INTO `news` VALUES (1,'news 1','this is the description of our fist news','2016-11-30 14:18:23'),(2,'news 2','this is the description of our second news','2016-11-30 14:24:23'),(3,'news 3','this is the description of our third news','2016-12-01 04:33:23');
-/*!40000 ALTER TABLE `news` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-12-12 17:55:24
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
